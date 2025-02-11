84371f892d24a315427a1b0aca7dc420774695fc
mustafawardeh <mostafa.wardeh2000@gmail.com>
2025-02-11 14:14:42 +0200

    add redis-with-next package and implement book retrieval from Redis in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,20 @@ import { client } from "@/lib/db";
 import Image from "next/image";
 import Link from "next/link";
 
+// Pipline to get books from Redis
+const getBooks = async () => {
+  const result = await client.zRangeWithScores("books", 0, -1);
+
+  const books = await Promise.all(
+    result.map((b) => {
+      return client.hGetAll(`books:${b.score}`)
+    })
+  )
+
+  return books;
+}
 export default async function Home() {
-  const books = await client.HGETALL("books:106172");
-  console.log(books);
+  const books: Record<string, string>[] = await getBooks()
   return (
     <main>
       <nav className="flex justify-between">
@@ -12,7 +23,16 @@ export default async function Home() {
         <Link href="/create" className="btn">Add a new book</Link>
       </nav>
 
-      <p>List of books here.</p>
+      {
+        books?.map((book) => (
+          <div key={book.id} className="card">
+            <h2>{book.title}</h2>
+            <p>By {book.author}</p>
+            <p>{book.blurb}</p>
+            <p>Rating: {book.rating}</p>
+          </div>
+        ))
+      }
     </main>
   );
 }
